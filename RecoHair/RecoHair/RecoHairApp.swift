2fd6cca793792b881a37b05238c0fa6c4033f3df
Sominjun <somongi@naver.com>
2025-05-07 15:43:34 +0900

    iOS Dev 브랜치 생성 완료

---
+++ RecoHair/RecoHair/RecoHairApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoHairApp.swift
+//  RecoHair
+//
+//  Created by 소민준 on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RecoHairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecoHair/RecoHairTests/RecoHairTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecoHairTests.swift
+//  RecoHairTests
+//
+//  Created by 소민준 on 5/7/25.
+//
+
+import Testing
+@testable import RecoHair
+
+struct RecoHairTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
